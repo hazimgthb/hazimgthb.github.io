4571421145a5137fdd6a33a87c8113ed79f2c966
Hazim <mdhazimar@gmail.com>
2020-12-11 21:17:32 +0800

    addedd changes

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -13,6 +13,12 @@ function getRandomUser(){
             let elEmail = document.getElementById("email");
             
             elFirstName.innerHTML = data.results[0].name.first;
+            elLastName.innerHTML = data.results[0].name.last;
+            elUserImage.src = data.results[0].picture.large;
+            elUserImage.title = data.results[0].name.first + " " + data.results[0].name.last;
+            elPhone.innerHTML = data.results[0].phone;
+            elAdress.innerHTML = data.results[0].location.street.number + " " + data.results[0].location.street.name + " " + data.results[0].location.city + " " + data.results[0].location.postcode + " " + data.results[0].location.state + " " + data.results[0].location.country;
+            elEmail.innerHTML = data.results[0].email;
 
             elJsonResult.innerHTML = this.response;
         }
