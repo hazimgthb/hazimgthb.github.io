cff25f9b9bd809f8e97ba770878f7a8af0c85a12
Hazim <mdhazimar@gmail.com>
2020-12-14 20:54:58 +0800

    added changes

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -4,3 +4,11 @@ let app = new Vue({
         message: " Hello Veu!"
     }
 });
+let app = new Veu({
+    el: "#app",
+    data: {
+        message: "you loaded this page on" + new Date().toLocaleString(),
+        linkURL: "https://bbc.in/2LAnJs8",
+        linkText: "Google is Down!"
+    }
+})
