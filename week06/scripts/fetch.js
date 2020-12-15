8f0ba1adad509b3c958a6450657c19b36b5e8b1c
Hazim <mdhazimar@gmail.com>
2020-12-15 21:33:57 +0800

    changes

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -27,3 +27,34 @@ let getBookingBtn = document.getElementById("getBooking");
 getBookingBtn.addEventListener("click", function () {
     GetBookings();
 });
+
+function BookNow(guestName, guestEmail, guestPax) {
+    let url = 'https://api.sheety.co/fabb7f1c61164decd1b0092f71c7786d/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with object
+            // console.log(json.booking);
+            let bookingMsg = document.getElementById("bookMsg");
+            bookingMsg.innerHTML = json.booking.name + " added!" GetBookings();
+        });
+}
+
+let bookNow = documet.getElementById("bookNow");
+bookNow.addEventListener("click", function () {
+    let gName = document.getElementById("guestName").value;
+    let gEmail = document.getElementById("guestEmail").value;
+    let gPax = document.getElementById("guestPax").value;
+
+    BookNow(gName, gEmail, gPax);
+})
