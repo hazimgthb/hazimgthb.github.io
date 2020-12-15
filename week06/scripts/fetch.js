89b6b5a5ece268c567b12ff737c9d67a7b730099
Hazim <mdhazimar@gmail.com>
2020-12-15 20:31:10 +0800

    changes

---
+++ week06/scripts/fetch.js
@@ -0,0 +1,9 @@
+function GetBookings() {
+    let url = 'https://api.sheety.co/fabb7f1c61164decd1b0092f71c7786d/bookingApp/bookings';
+    fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            console.log(json.bookings);
+        });
+}
