3cc44839c97fb106e01098953b87153becb029a2
Hazim <mdhazimar@gmail.com>
2020-12-22 20:16:22 +0800

    added JS

---
+++ tableapp/scripts/book.js
@@ -0,0 +1,33 @@
+function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
+    let url = 'https://api.sheety.co/fabb7f1c61164decd1b0092f71c7786d/bookingApp/sheet1';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax,
+            remarks: guestRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert(json.booking.name + " added!")
+        });
+}
+
+window.addEventListener("load", function () {
+    document.getElementById("bookNow").addEventListener("click", function () {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userEmail").value;
+        let userPax = document.getElementById("userPax").value;
+        let userRemarks = document.getElementById("userRemarks").value;
+
+        BookNow(userName, userEmail, userPax, userRemarks)
+    })
+});
